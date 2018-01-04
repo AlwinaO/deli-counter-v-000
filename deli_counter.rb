5d0ae7858b11b93859f579cbeb45d4fbcc65bdab
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-01-04 00:32:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,3 +19,10 @@ def take_a_number(katz_deli, name)
   katz_deli << name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving
+  if line.empty?
+
+  else
+  puts '#{katz_deli.first}'
+end
