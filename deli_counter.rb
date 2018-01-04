f5d12ebdf3a147c8711d30891714deb119f83656
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-01-04 00:52:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,13 +5,13 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
 
-  elsif other_deli.length >= 1
-    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
+  else
+    current_line = "The line is currently:"
+    katz_deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
 
-  elsif another_deli.first
-
-    puts "The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey"
-  end
 end
 
 def take_a_number(katz_deli, name)
