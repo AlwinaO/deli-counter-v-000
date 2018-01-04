1b0f9f14337896dbb08f3c0e4b4cc59cab345780
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-01-04 00:34:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,11 +20,11 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving
+def now_serving(katz_deli)
   if line.empty?
-
+    "There is nobody waiting to be served!"
   else
-  puts '#{katz_deli.first}'
+    puts '#{katz_deli.first}'
 end
 
 end
