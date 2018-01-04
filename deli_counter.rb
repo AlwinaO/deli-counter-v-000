0a2c1105ab35bc5a68d01065bb6c9fac2d8204db
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-01-04 00:41:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ end
 
 def now_serving(katz_deli)
   if line.empty?
-    "There is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   else
     puts '#{katz_deli.first}'
 end
